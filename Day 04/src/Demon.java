54a15ac4cf01a0a114e135a1a00bce8003c3a0c2
Haarish <Divya@LAPTOP-OTKKHLQQ>
2022-06-05 13:50:37 +0530

    First commit

---
+++ Day 04/src/Demon.java
@@ -0,0 +1,13 @@
+
+public class Demon {
+int x;
+}
+	class Testdemon{	
+	public static void main(String[] args) {
+		Demon myobj = new Demon();
+		myobj.x = 40;
+		System.out.println(myobj.x);
+
+	}
+
+}
