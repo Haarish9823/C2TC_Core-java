08d36c7171ac4d99327cdb6264256a39c0183a20
haarish <Divya@LAPTOP-OTKKHLQQ>
2022-08-09 14:14:40 +0530

    commit

---
+++ Day 20/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static void splitString(String text) {
+		try {
+		String[] splittedString =text.split("/");
+		for(int i = 0; i < splittedString.length; i++)
+		{
+		System.out.println("Splitted string at index "+i+" is:"+splittedString[i]);
+		}}
+		catch(Exception e){
+		System.out.println("Exception : "+e);
+		}
+		finally{
+		System.out.println("Inside finally block");
+		}}
+		public static void main(String args[])
+		{
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine();
+		if(text.length()>2)
+		{
+		splitString(text);
+		}
+		else
+		{
+		splitString(null);
+	}
+}}
