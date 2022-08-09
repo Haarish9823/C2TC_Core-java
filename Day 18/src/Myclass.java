ddfbfd7e9bf3108f2566412f3d04f953efcd0591
haarish <Divya@LAPTOP-OTKKHLQQ>
2022-08-09 14:01:17 +0530

    first commit

---
+++ Day 18/src/Myclass.java
@@ -0,0 +1,21 @@
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		try{
+			int num=121/0;
+			System.out.println(num);
+			}
+			catch(ArithmeticException e){
+			System.out.println("Number should not be divided by zero");
+			}
+			finally{
+			System.out.println("This is finally block");
+			}
+			System.out.println("Out of try-catch-finally");
+	}
+}
+// output
+// Number should not be divided by zero
+// This is finally block
+// Out of try-catch-finally
---
+++ Day 18/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		try{
+			int i;
+			return;
+			}
+			catch(Exception e){
+			System.out.print("CatchBlock");
+			}
+			finally{
+			System.out.println("FinallyBlock");
+			}
+	}
+}
+// FinallyBlock
