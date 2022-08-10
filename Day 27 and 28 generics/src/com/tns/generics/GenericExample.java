8370965ee687c0d8241a6ced29cb83ec843a27a3
haarish <Divya@LAPTOP-OTKKHLQQ>
2022-08-10 14:58:17 +0530

    commit

---
+++ Day 27 and 28 generics/src/com/tns/generics/GenericExample.java
@@ -0,0 +1,30 @@
+package com.tns.generics;
+class Dataa{
+	private Object str;
+
+	public Object getStr() {
+		return str;
+	}
+
+	public Dataa(Object str) {
+		super();
+		this.str = str;
+	}
+
+	@Override
+	public String toString() {
+		return "Dataa [str=" + str + ", getStr()=" + getStr() + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+}
+public class GenericExample {
+
+	public static void main(String[] args) {
+		Dataa obj = new Dataa(3.876666);
+		String output =(String) obj.getStr();
+		System.out.println(output);
+
+	}
+
+}
