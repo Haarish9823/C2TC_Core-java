c91511f1b89d042de6f741886a3639ddedb132f1
Haarish <Divya@LAPTOP-OTKKHLQQ>
2022-08-12 14:16:51 +0530

    commit

---
+++ AcharyaTableperclassinheritance/src/com/cg/entities/Manager.java
@@ -0,0 +1,27 @@
+package com.cg.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="mfr_store")
+public class Manager extends Employee{
+
+private static final long serialVersionUID=1L;
+	
+	private String departmentName;
+
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+}
