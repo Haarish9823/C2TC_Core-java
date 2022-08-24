d4fcc375ea004aa0d1ccf4bfb743ab4a00bafbd3
Haarish <Divya@LAPTOP-OTKKHLQQ>
2022-08-24 18:09:45 +0530

    commit

---
+++ acharyademo/src/main/java/com/cg/springioc/Airtel.java
@@ -0,0 +1,14 @@
+package com.cg.springioc;
+
+public class Airtel implements Sim{
+
+	public void data() {
+		System.out.println("browsing using Airtel");
+	}
+
+	public void calling() {
+		System.out.println("calling using Airtel");
+		
+	}
+
+}
---
+++ acharyademo/src/main/java/com/cg/springioc/Sim.java
@@ -0,0 +1,7 @@
+package com.cg.springioc;
+
+public interface Sim {
+	void data();
+	void calling();
+
+}
